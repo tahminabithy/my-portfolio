d387172d2115614e96225f23cc6d50501c950788
tahminabithy <tahmina.akter9519@gmail.com>
2024-03-02 18:05:45 -0500

    added contact page

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { useForm } from "react-hook-form";
+export default function Contact() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div id="contact" className="px-8 py-6 md:px-24 md:py-12">
+      <h2 className="  about-heading">Contacts</h2>
+      <div className="md:p-12 grid grid-cols-1 md:grid-cols-2 gap-8">
+        {" "}
+        <div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              placeholder="Name"
+              className="w-100 py-3 px-4 border-1 border-black  rounded-3xl bg-pink-800"
+              {...register("name", { required: true })}
+            />
+            <br />
+            <input
+              className="mt-10  w-100 py-3 px-4 border-1 border-black rounded-3xl bg-pink-800"
+              placeholder="Email"
+              {...register("email")}
+            />
+            <br />
+            <textarea
+              placeholder="Type your message ...."
+              className="mt-10 h-56  w-100 py-3 px-4  border-1 border-black rounded-3xl bg-pink-800"
+              {...register("message")}
+            />
+            <input
+              className="mt-10 px-12 py-2 border-3 text-pink-800 font-bold rounded-3xl border-pink-800"
+              type="submit"
+            />
+          </form>
+        </div>
+        <div className="text-white md:px-16">
+          <p className="font-bold text-lg">
+            <i className="fas fa-envelope  mr-8 bg-pink-800 p-3 rounded-full text-black shadow-lg  "></i>{" "}
+            tahmina.akter9519@gmail.com
+          </p>
+          <p className="font-bold text-lg">
+            <i className="fas fa-phone mr-8 bg-pink-800 p-3 rounded-full text-black "></i>
+            +514-577-4959
+          </p>
+          <p className="font-bold text-lg">
+            <i className="fas fa-location-arrow mr-8 bg-pink-800 p-3 rounded-full text-black "></i>
+            Montreal , Quebec
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,38 +1,56 @@
-import React, { useState } from 'react'
-import { Button, Nav, Offcanvas } from 'react-bootstrap'
-import './Header.css'
-import img from '../image/tahmina.jpg'
-import About from '../about/About'
-import Education from '../education/Education'
+import React, { useState } from "react";
+import { Button, ButtonToolbar, Nav, Offcanvas } from "react-bootstrap";
+import "./Header.css";
+import img from "../image/tahmina.jpg";
+import About from "../about/About";
+import Education from "../education/Education";
 //import { HashLink as Link } from 'react-router-hash-link'
-import { Link, animateScroll as scroll } from 'react-scroll'
-import { HashLink } from 'react-router-hash-link'
-import Project from '../projects/Project'
+import { Link, animateScroll as scroll } from "react-scroll";
+import { HashLink } from "react-router-hash-link";
+import Project from "../projects/Project";
+import Contact from "../Contact/Contact";
 const options = [
   {
-    name: 'Enable both scrolling & backdrop',
+    name: "Enable both scrolling & backdrop",
     scroll: true,
     backdrop: true,
   },
-]
+];
 
 const Header = ({ name, ...props }) => {
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false)
+  const handleClose = () => setShow(false);
 
-  const toggleShow = () => setShow((s) => !s)
+  const toggleShow = () => setShow((s) => !s);
+
+  // ---------------------
+  const onButtonClick = () => {
+    // using Java Script method to get PDF file
+    fetch("Tahmina_Akter_cv.pdf").then((response) => {
+      response.blob().then((blob) => {
+        // Creating new object of PDF file
+        const fileURL = window.URL.createObjectURL(blob);
+
+        // Setting various property values
+        let alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "SamplePDF.pdf";
+        alink.click();
+      });
+    });
+  };
   return (
     <section>
-      <div className='p-5' id='home'>
-        <div className='d-flex justify-content-between '>
-          <p className='logo-font text-white'>Tahmina Akter</p>
+      <div className="p-5" id="home">
+        <div className="d-flex justify-content-between ">
+          <p className="logo-font text-white">Tahmina Akter</p>
           <div>
-            <Button variant='dark' onClick={toggleShow} className='me-2'>
-              <i class='fas fa-bars fs-4 '></i>
+            <Button variant="dark" onClick={toggleShow} className="me-2">
+              <i class="fas fa-bars fs-4 "></i>
             </Button>
             <Offcanvas
-              className='bg-dark offcanvas-size'
+              className="bg-dark offcanvas-size"
               show={show}
               onHide={handleClose}
               {...props}
@@ -40,7 +58,7 @@ const Header = ({ name, ...props }) => {
               <Offcanvas.Header closeButton>
                 <Offcanvas.Title>Offcanvas</Offcanvas.Title>
               </Offcanvas.Header>
-              <Offcanvas.Body className='text-white rounded-3 d-flex justify-content-center'>
+              <Offcanvas.Body className="text-white rounded-3 d-flex justify-content-center">
                 <ul>
                   <li>
                     {/* <Nav.Link
@@ -52,54 +70,54 @@ const Header = ({ name, ...props }) => {
                     </Nav.Link> */}
 
                     <a
-                      className='text-decoration-none font-style menu-style '
-                      href='#home'
+                      className="text-decoration-none font-style menu-style "
+                      href="#home"
                     >
-                      <i class='fas fa-home pe-2'></i> Home
+                      <i class="fas fa-home pe-2"></i> Home
                     </a>
                   </li>
-                  <li className=' my-5'>
+                  <li className=" my-5">
                     {/* <HashLink smooth to='/home#about'>
                       about
                     </HashLink>
                     ; */}
                     <a
-                      className='text-decoration-none font-style menu-style my-5 px-4'
-                      href='#about'
+                      className="text-decoration-none font-style menu-style my-5 px-4"
+                      href="#about"
                     >
-                      <i class='fas fa-user pe-1'></i>About
+                      <i class="fas fa-user pe-1"></i>About
                     </a>
                   </li>
-                  <li className=' mb-5'>
+                  <li className=" mb-5">
                     <a
-                      className='text-decoration-none font-style menu-style'
-                      href='#project'
+                      className="text-decoration-none font-style menu-style"
+                      href="#project"
                     >
-                      <i class='fas fa-shield-virus pe-1'></i> Projects
+                      <i class="fas fa-shield-virus pe-1"></i> Projects
                     </a>
                   </li>
-                  <li className='my-5'>
+                  <li className="my-5">
                     <a
-                      className='text-decoration-none font-style menu-style'
-                      href='#education'
+                      className="text-decoration-none font-style menu-style"
+                      href="#education"
                     >
-                      <i class='fas fa-file pe-2'></i>Resume
+                      <i class="fas fa-file pe-2"></i>Resume
                     </a>
                   </li>
                   <li>
                     <a
-                      className='text-decoration-none font-style menu-style'
-                      href='#contact'
+                      className="text-decoration-none font-style menu-style"
+                      href="#contact"
                     >
-                      <i class='fas fa-phone-square-alt '></i> Contact
+                      <i class="fas fa-phone-square-alt "></i> Contact
                     </a>
                   </li>
-                  <li className=' my-5'>
+                  <li className=" my-5">
                     <a
-                      className='text-decoration-none menu-style font-style px-4'
-                      href='#skill'
+                      className="text-decoration-none menu-style font-style px-4"
+                      href="#skill"
                     >
-                      <i class='fas fa-cog pe-2'></i> Skills
+                      <i class="fas fa-cog pe-2"></i> Skills
                     </a>
                   </li>
                 </ul>
@@ -107,27 +125,27 @@ const Header = ({ name, ...props }) => {
             </Offcanvas>
           </div>
         </div>
-        <div className='row g-4'>
-          <div className='col-lg-4 col-md-4 col-12 header-bg1 d-flex justify-content-center align-items-center pro-pic'>
+        <div className="row g-2">
+          <div className="col-lg-4 col-md-4 col-12 header-bg1 d-flex justify-content-center align-items-center pro-pic">
             <div>
               <div>
                 <img
-                  className='img-fluid rounded-circle img-size'
+                  className="img-fluid rounded-circle img-size"
                   src={img}
-                  alt=''
+                  alt=""
                 />
               </div>
-              <div className='mt-5'>
-                <img src='https://img.icons8.com/fluency-systems-filled/48/000000/github.png' />
-                <img src='https://img.icons8.com/ios-glyphs/48/000000/linkedin.png' />
-                <img src='https://img.icons8.com/ios-filled/48/000000/facebook-new.png' />
+              <div className="mt-5 flex justify-center items-center">
+                <img src="https://img.icons8.com/fluency-systems-filled/48/000000/github.png" />
+                <img src="https://img.icons8.com/ios-glyphs/48/000000/linkedin.png" />
+                <img src="https://img.icons8.com/ios-filled/48/000000/facebook-new.png" />
               </div>
             </div>
           </div>
-          <div className='col-lg-8 col-md-8 col-12 header-bg2 text-start'>
-            <p className='text-white font-style fs-4'>Web Developer</p>
-            <h1 className='mb-4 name'>Tahmina Bithy</h1>
-            <p className='text-white text-wrap font-style fs-6'>
+          <div className="col-lg-8 col-md-8 col-12 header-bg2 text-start">
+            <p className="text-white font-style fs-4">Web Developer</p>
+            <h1 className="mb-4 name">Tahmina Akter</h1>
+            <p className="text-white text-wrap font-style fs-6">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad
               veritatis necessitatibus dolorum amet ipsa sunt dolorem id
               exercitationem nam illum totam nisi assumenda, voluptatem sint in.
@@ -135,14 +153,30 @@ const Header = ({ name, ...props }) => {
               ad maiores in ipsa, consequatur suscipit sed cupiditate! Quae
               ratione repudiandae tempora velit, enim non sit!
             </p>
+            <div className="block sm:flex flex-row mt-8">
+              {" "}
+              <button
+                onClick={onButtonClick}
+                className="px-4 py-2 font-bold rounded-3xl text-pink-800 border-2  border-pink-800"
+              >
+                Download CV
+              </button>
+              {/* <button onClick={onButtonClick}>Download PDF</button> */}
+              <button className="mt-2 sm:mt-0 ml-2 md:ml-8 px-4 py-2 font-semibold rounded-3xl  border-2 border-pink-800  bg-pink-800">
+                <a className="text-decoration-none text-black" href="#contact">
+                  Contact
+                </a>
+              </button>
+            </div>
           </div>
         </div>
       </div>
       <About />
       <Education />
       <Project />
+      <Contact />
     </section>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
